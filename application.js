b37c3881cd0ca90efae484923869cf9acf7af142
Alexey Vylegzhanin <alexey.vylegzhanin@yandex.ru>
2017-05-12 19:02:36 +0300

    Start JavaScript

---
+++ application.js
@@ -0,0 +1,18 @@
+var people = [];
+var template = $('#people-template').html();
+
+$('#peopleModule').find('button').on('click', function() {
+	people.push( $('#peopleModule').find('input').val() );
+	$('#peopleModule').find('input').val('');
+
+	var data = {
+		people: people
+	};
+
+	$('#peopleModule').find('ul').html(Mustache.render(template, data));
+});
+
+$('#peopleModule').find('ul').delegate('i.del', 'click', function(e) {
+	var $remove = $(e.target).closest('li');
+	var i = $('#peopleModule').find('li').index($remove);
+});
